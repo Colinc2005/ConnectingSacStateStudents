752035790b4fc6f2571639e71907be4295b3ed27
Benjamin Chong <bchong@csus.edu>
2025-11-26 21:25:12 -0800

    cleaned stuff up
    worked on chat and css

--- sac_state_connect_db.sql
+++
@@ -1,9 +0,0 @@
-create database sac_state_student_info;
-use sac_state_student_info; 
-
-create table students (
-id int auto_increment primary key,
-name varchar(50) not null,
-major varchar(50),
-account_created timestamp default current_timestamp
-)
